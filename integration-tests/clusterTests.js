582378c191f6a61aaecb60b6c47cd76c0cf78ac8
Joseph Zack <jzack@baydynamics.net>
2019-11-14 17:48:16 -0500

    Trying to run kafka locally for testing

--- integration-tests/clusterTests.js
+++ integration-tests/clusterTests.js
@@ -5,38 +5,65 @@ const url = process.env.GRAPHQL_ENDPOINT || `http://localhost:9000/`; // TODO
 
 const request = require("supertest")(url);
 
+const validLocalConfig = `application.id=grafka
+group.id=grafka
+bootstrap.servers=kafka1:9092
+
+schema.registry.url=http://schemaregistry:8081
+
+key.serializer=org.apache.kafka.common.serialization.StringSerializer
+value.serializer=io.confluent.kafka.serializers.KafkaAvroSerializer
+
+key.deserializer=org.apache.kafka.common.serialization.StringDeserializer
+value.deserializer=io.confluent.kafka.serializers.KafkaAvroDeserializer
+
+avro.specific.reader=true`;
+
+const basicClusterQuery = `
+{
+  clusters {
+    clusterId
+    config
+    description {
+      nodes {
+        id
+        host
+        port
+        rack
+      }
+      controller {
+        id
+        host
+        port
+        rack
+      }
+      authorizedOperations {
+        code
+        unknown
+      }
+    }
+  }
+}`;
+
+const newClusterMutation = `mutation($name:String!, $config:String!) {
+  newCluster(name: $name, config: $config) {
+    clusterId
+    name
+    config
+  }
+}`;
+
+// TODO Got to get rid of this, how to better check that kafka is ready?
+require('system-sleep')(60 * 1000);
+
 describe("Grafka with no saved clusters", () => {
   it("returns an empty array of clusters", done => {
     request
       .post("/graphql")
       .send({
-        query: `
-          {
-            clusters {
-              clusterId
-              config
-              description {
-                nodes {
-                  id
-                  host
-                  port
-                  rack
-                }
-                controller {
-                  id
-                  host
-                  port
-                  rack
-                }
-                authorizedOperations {
-                  code
-                  unknown
-                }
-              }
-            }
-          }
-        `
+        query: basicClusterQuery
       })
+      .timeout(2 * 60 * 1000)
       .expect(200)
       .end((err, res) => {
         // res will contain array with one user
@@ -47,5 +74,31 @@ describe("Grafka with no saved clusters", () => {
         //res.body.data.should.have.property("clusters");
         done();
       });
+  });
+
+  it("saves a new valid cluster", done => {
+    request
+        .post("/graphql")
+        .send({
+          query: newClusterMutation,
+          variables: {
+            name: "test",
+            config: validLocalConfig
+          }
+        })
+        .timeout(2 * 60 * 1000)
+        .expect(200)
+        .end((err, res) => {
+          // res will contain array with one user
+          if (err) return done(err);
+          const data = res.body.data;
+          console.log(data)
+          data.should.have.property("newCluster");
+
+          assert.equal("test", data.newCluster.name);
+          assert.equal(36, data.newCluster.clusterId.length);
+          //res.body.data.should.have.property("clusters");
+          done();
+        });
   });
 });
